3c3974a37bd16a56a51794d94ce1df65d819fa1c
Arumyan <alekseyrumyancev@mail.ru>
2018-11-11 10:52:50 +0300

    add new component

---
+++ src/components/NewQuote.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row">
+    <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+      <label for="">Quote</label>
+      <textarea class="form-control" rows="3" v-model="quote"></textarea>
+    </div>
+
+    <div class="col-sm-8 col-sm-offset-2 col-xs-12 col-md-6 col-md-offset-3">
+      <button class="btn btn-success" @click.prevent="createNew">Add Quote</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './Quote.vue';
+export default {
+  data() {
+    return {
+      quote: ''
+    }
+  },
+  methods: {
+    createNew() {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
+
